30d72c96258253d6d3fc23ce5d870d26ca687fbd
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-02-01 17:20:19 -0600

    modified to include keyword search

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,22 +6,37 @@ app.controller('GiphyController', function($http){
   var API = 'http://api.giphy.com/v1';
 
   var ctrl = this;
-   ctrl.imageUrl = " ";
-
+  ctrl.imageUrl = " ";
+  ctrl.searchQuery = " ";
+  ctrl.search =[];
   ctrl.addRandomGiphy = function(){
     $http ({
-    method:'GET',
+      method:'GET',
       url: API + '/gifs/random',
       params : {
         api_key:'dc6zaTOxFJmzC',
         q:null
       }
     }).then(function(response){
-    console.log('Got a response from the API', response);
-    ctrl.imageUrl = response.data.data.image_url;
-  }).catch(function(err){
-    console.log('Error getting info from API', err);
-  });
-}
-
+      console.log('Got a response from the API', response);
+      ctrl.imageUrl = response.data.data.image_url;
+    }).catch(function(err){
+      console.log('Error getting info from API', err);
+    });
+  };
+  ctrl.searchForGiphy = function(){
+    $http ({
+      method:'GET',
+      url: API + '/gifs/search',
+      params : {
+        api_key:'dc6zaTOxFJmzC',
+        q:ctrl.searchQuery
+      }
+    }).then(function(response){
+      console.log('Got a response from the API', response);
+      ctrl.search = response.data.data;
+    }).catch(function(err){
+      console.log('Error getting info from API', err);
+    });
+  };
 });
